f157f10fdad362991d3c889643c87ffb5ec91e7a
WangJi92 <983433479@qq.com>
2018-07-17 20:04:12 +0800

    mybaits LocalCacheScope 本地缓存作用域，也就是一级缓存是配置在当前查询中还是一个Sqlsession中

--- src/main/java/org/apache/ibatis/session/LocalCacheScope.java
+++ src/main/java/org/apache/ibatis/session/LocalCacheScope.java
@@ -16,8 +16,25 @@
 package org.apache.ibatis.session;
 
 /**
+ * 本地缓存作用域
+ *
+ * 一级缓存是指SqlSession级别的缓存，当在同一个SqlSession中进行相同的SQL语句查询时，第二次以后的查询不会从数据库查询，而是直接从缓存中获取，一级缓存最多缓存1024条SQL。
+ * 二级缓存是指可以跨SqlSession的缓存。
+ *
+ * <p>[Mybatis Local Cache陷阱](https://segmentfault.com/a/1190000008207977)
+ * [MyBatis（3.4.2）的Cache机制完全解析](https://blog.csdn.net/realskyzou/article/details/54137051)
+ *
+ * [Mybatis缓存介绍-这个说得清楚一点](https://blog.csdn.net/u010643307/article/details/70148723)
+ * </p>
  * @author Eduardo Macarron
  */
 public enum LocalCacheScope {
-  SESSION,STATEMENT
+  /**
+   * 一级缓存保存在session中还是在一个查询语句中？当前是session
+   */
+  SESSION,
+  /**
+   * 一级缓存保存在STATEMENT中去处理哦
+   */
+  STATEMENT
 }
