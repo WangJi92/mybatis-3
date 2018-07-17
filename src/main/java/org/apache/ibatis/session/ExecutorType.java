fe7edf910bcee21e1ae1bc112da391fde00d2610
WangJi92 <983433479@qq.com>
2018-07-17 19:52:18 +0800

    mybaits 配置中执行器常量

--- src/main/java/org/apache/ibatis/session/ExecutorType.java
+++ src/main/java/org/apache/ibatis/session/ExecutorType.java
@@ -16,8 +16,22 @@
 package org.apache.ibatis.session;
 
 /**
+ * 配置和设定执行器，SIMPLE 执行器执行其它语句。REUSE 执行器可能重复使用prepared statements 语句，BATCH执行器可以重复执行语句和批量更新。
+ * [MyBatis配置详解](https://blog.csdn.net/bear_wr/article/details/52401881)
+ * [mybatis使用的一点小结：session运行模式及批量提交](https://blog.csdn.net/meiwen1111/article/details/8260387)
  * @author Clinton Begin
  */
 public enum ExecutorType {
-  SIMPLE, REUSE, BATCH
+  /**
+   * 这个类型不做特殊的事情，它只为每个语句创建一个PreparedStatement
+   */
+  SIMPLE,
+  /**
+   * 这种类型将重复使用PreparedStatements
+   */
+  REUSE,
+  /**
+   * 这个类型批量更新，且必要地区别开其中的select 语句，确保动作易于理解
+   */
+  BATCH
 }
