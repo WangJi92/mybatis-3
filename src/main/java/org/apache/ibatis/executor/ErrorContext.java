51f03f763e9966d4b354fceeaf2e10819e11be5c
WangJi92 <983433479@qq.com>
2018-07-17 19:37:52 +0800

    规范化打印错误日志的信息，链式调用逻辑，看起来好看哦~~~

--- src/main/java/org/apache/ibatis/executor/ErrorContext.java
+++ src/main/java/org/apache/ibatis/executor/ErrorContext.java
@@ -16,24 +16,47 @@
 package org.apache.ibatis.executor;
 
 /**
+ * 错误信息收集，并集中处理打印错误信息，规范一下打印日志的信息
  * @author Clinton Begin
  */
 public class ErrorContext {
 
   private static final String LINE_SEPARATOR = System.getProperty("line.separator","\n");
-  private static final ThreadLocal<ErrorContext> LOCAL = new ThreadLocal<>();
 
+  /**
+   * [自己曾经写的博客ThreadLocal](https://blog.csdn.net/u012881904/article/details/80100228)
+   */
+  private static final ThreadLocal<ErrorContext> LOCAL = new ThreadLocal<>();
+  /**
+   * 在不同线程线程之间非冲突的使用打印错误的信息
+   */
   private ErrorContext stored;
+
   private String resource;
   private String activity;
   private String object;
+  /**
+   * 打印错误的信息
+   */
   private String message;
+
+  /**
+   * 需要打印的SQL
+   */
   private String sql;
+
+  /**
+   * 异常的原因
+   */
   private Throwable cause;
 
   private ErrorContext() {
   }
 
+  /**
+   * 创建一个实例，放置在当前线程之中去处理
+   * @return
+   */
   public static ErrorContext instance() {
     ErrorContext context = LOCAL.get();
     if (context == null) {
@@ -43,12 +66,20 @@ public class ErrorContext {
     return context;
   }
 
+  /**
+   * 保存到当前线程使用哦
+   * @return
+   */
   public ErrorContext store() {
     stored = this;
     LOCAL.set(new ErrorContext());
     return LOCAL.get();
   }
 
+  /**
+   * 将之前保存的数据，从新放置到当前线程使用
+   * @return
+   */
   public ErrorContext recall() {
     if (stored != null) {
       LOCAL.set(stored);
@@ -87,6 +118,10 @@ public class ErrorContext {
     return this;
   }
 
+  /**
+   * 销毁当前的数据信息
+   * @return
+   */
   public ErrorContext reset() {
     resource = null;
     activity = null;
@@ -98,6 +133,10 @@ public class ErrorContext {
     return this;
   }
 
+  /**
+   * 规范一下打印错误的信息
+   * @return
+   */
   @Override
   public String toString() {
     StringBuilder description = new StringBuilder();
