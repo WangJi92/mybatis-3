008a318653a753473af18c7f67eb57542d98fb67
WangJi92 <983433479@qq.com>
2018-07-18 20:01:39 +0800

    mybaits 主键处理策略

--- src/main/java/org/apache/ibatis/executor/keygen/KeyGenerator.java
+++ src/main/java/org/apache/ibatis/executor/keygen/KeyGenerator.java
@@ -15,12 +15,19 @@
  */
 package org.apache.ibatis.executor.keygen;
 
-import java.sql.Statement;
-
 import org.apache.ibatis.executor.Executor;
 import org.apache.ibatis.mapping.MappedStatement;
 
+import java.sql.Statement;
+
 /**
+ * <P>
+ *     [MyBatis主键生成器KeyGenerator（一）](https://blog.csdn.net/qq924862077/article/details/52673430)
+ *      Mybatis提供了主键生成器接口KeyGenerator，insert语句默认是不返回记录的主键值，而是返回插入的记录条数；
+ *      如果业务层需要得到记录的主键时，可以通过配置的方式来完成这个功能
+ * </P>
+ * （1）针对Sequence主键而言，在执行insert sql前必须指定一个主键值给要插入的记录，如Oracle、DB2，KeyGenerator提供了processBefore（）方法。
+ * （2）针对自增主键的表，在插入时不需要主键，而是在插入过程自动获取一个自增的主键，比如MySQL，Postgresql，KeyGenerator提供了processAfter（）方法。
  * @author Clinton Begin
  */
 public interface KeyGenerator {
