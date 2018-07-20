d0c1a17c7ce22ec06505ced640aa90218b5044d6
WangJi92 <983433479@qq.com>
2018-07-20 14:04:15 +0800

    这个就是通过反射创建对象信息的一个类的处理哦，具体实例可以查看  DefaultObjectFactoryTest

--- src/main/java/org/apache/ibatis/reflection/factory/DefaultObjectFactory.java
+++ src/main/java/org/apache/ibatis/reflection/factory/DefaultObjectFactory.java
@@ -15,22 +15,21 @@
  */
 package org.apache.ibatis.reflection.factory;
 
-import java.io.Serializable;
-import java.lang.reflect.Constructor;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Properties;
-import java.util.Set;
-import java.util.SortedSet;
-import java.util.TreeSet;
-
 import org.apache.ibatis.reflection.ReflectionException;
 
+import java.io.Serializable;
+import java.lang.reflect.Constructor;
+import java.util.*;
+
 /**
+ * 这个就是通过反射创建对象信息的一个类的处理哦，具体实例可以查看  DefaultObjectFactoryTest
+ * <p>
+ *      [Mybatis-ObjectFactory](https://blog.csdn.net/qq_28796345/article/details/64923725)
+ *      [Mybatis3.3.x技术内幕（七）：Mybatis初始化之六个工具](https://my.oschina.net/zudajun/blog/668596)
+ * </p>
+ *
+ *
+ *
  * @author Clinton Begin
  */
 public class DefaultObjectFactory implements ObjectFactory, Serializable {
@@ -45,8 +44,10 @@ public class DefaultObjectFactory implements ObjectFactory, Serializable {
   @SuppressWarnings("unchecked")
   @Override
   public <T> T create(Class<T> type, List<Class<?>> constructorArgTypes, List<Object> constructorArgs) {
+    //看看是否为特殊类型的类信息
     Class<?> classToCreate = resolveInterface(type);
-    // we know types are assignable
+
+    // we know types are assignable  然后通过构造函数 和参数进行反射
     return (T) instantiateClass(classToCreate, constructorArgTypes, constructorArgs);
   }
 
@@ -58,6 +59,8 @@ public class DefaultObjectFactory implements ObjectFactory, Serializable {
   private  <T> T instantiateClass(Class<T> type, List<Class<?>> constructorArgTypes, List<Object> constructorArgs) {
     try {
       Constructor<T> constructor;
+
+      //通过默认构造函数创建一个对象
       if (constructorArgTypes == null || constructorArgs == null) {
         constructor = type.getDeclaredConstructor();
         if (!constructor.isAccessible()) {
@@ -65,19 +68,23 @@ public class DefaultObjectFactory implements ObjectFactory, Serializable {
         }
         return constructor.newInstance();
       }
+
+      // 通过传递的构造函数进行创建
       constructor = type.getDeclaredConstructor(constructorArgTypes.toArray(new Class[constructorArgTypes.size()]));
       if (!constructor.isAccessible()) {
         constructor.setAccessible(true);
       }
       return constructor.newInstance(constructorArgs.toArray(new Object[constructorArgs.size()]));
     } catch (Exception e) {
+      //region 构造异常，通过获取类信息打印处理异常信息
       StringBuilder argTypes = new StringBuilder();
       if (constructorArgTypes != null && !constructorArgTypes.isEmpty()) {
         for (Class<?> argType : constructorArgTypes) {
           argTypes.append(argType.getSimpleName());
           argTypes.append(",");
         }
-        argTypes.deleteCharAt(argTypes.length() - 1); // remove trailing ,
+        argTypes.deleteCharAt(argTypes.length() - 1);
+        // remove trailing ,
       }
       StringBuilder argValues = new StringBuilder();
       if (constructorArgs != null && !constructorArgs.isEmpty()) {
@@ -85,12 +92,19 @@ public class DefaultObjectFactory implements ObjectFactory, Serializable {
           argValues.append(String.valueOf(argValue));
           argValues.append(",");
         }
-        argValues.deleteCharAt(argValues.length() - 1); // remove trailing ,
+        argValues.deleteCharAt(argValues.length() - 1);
+        // remove trailing ,
       }
+      //endregion
       throw new ReflectionException("Error instantiating " + type + " with invalid types (" + argTypes + ") or values (" + argValues + "). Cause: " + e, e);
     }
   }
 
+  /**
+   * 处理一些特殊的类型，比如 List Collection 等等，给一个特定的类型去处理
+   * @param type
+   * @return
+   */
   protected Class<?> resolveInterface(Class<?> type) {
     Class<?> classToCreate;
     if (type == List.class || type == Collection.class || type == Iterable.class) {
--- src/main/java/org/apache/ibatis/reflection/factory/ObjectFactory.java
+++ src/main/java/org/apache/ibatis/reflection/factory/ObjectFactory.java
@@ -19,6 +19,7 @@ import java.util.List;
 import java.util.Properties;
 
 /**
+ * 使用一个ObjectFactory来创建所有需要的新对象。【就是使用来简化通过反射创建的过程】
  * MyBatis uses an ObjectFactory to create all needed new Objects.
  * 
  * @author Clinton Begin
@@ -26,12 +27,14 @@ import java.util.Properties;
 public interface ObjectFactory {
 
   /**
+   * 设置配置属性。
    * Sets configuration properties.
    * @param properties configuration properties
    */
   void setProperties(Properties properties);
 
   /**
+   * 使用默认构造函数创建一个新的对象。
    * Creates a new object with default constructor. 
    * @param type Object type
    * @return
@@ -39,20 +42,23 @@ public interface ObjectFactory {
   <T> T create(Class<T> type);
 
   /**
+   * 创建一个新对象的构造函数和参数指定
    * Creates a new object with the specified constructor and params.
-   * @param type Object type
-   * @param constructorArgTypes Constructor argument types
-   * @param constructorArgs Constructor argument values
+   * @param type Object type  需要创建对象的类型
+   * @param constructorArgTypes Constructor argument types 构造函数参数类型
+   * @param constructorArgs Constructor argument values  构造函数参数值
    * @return
    */
   <T> T create(Class<T> type, List<Class<?>> constructorArgTypes, List<Object> constructorArgs);
   
   /**
+   * 返回true,如果这个对象可以有一组的其他对象
    * Returns true if this object can have a set of other objects.
+   * 这个主要是为了支持非Java 数据 比如scala 的集合对象信息
    * It's main purpose is to support non-java.util.Collection objects like Scala collections.
    * 
-   * @param type Object type
-   * @return whether it is a collection or not
+   * @param type Object type 对象类型
+   * @return whether it is a collection or not 是否它是一个集合
    * @since 3.1.0
    */
   <T> boolean isCollection(Class<T> type);
